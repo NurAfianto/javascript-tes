d245de2cd1d84a15a3437732055c615e539b81a8
NurAfianto <nurafyanto2108@gmail.com>
2023-12-25 00:31:08 +0700

    make ruler bottom

--- js/layup-drawer.js
+++ js/layup-drawer.js
@@ -26,6 +26,9 @@ LayupDrawer.prototype = {
     drawLayup : function (layup, length) {
         let canvas = this.canvas;
         let ctx = canvas.getContext('2d');
+        let paddingLeft = 40;
+        let paddingRight = 60;
+        let canvasWidth = canvas.width - paddingLeft - paddingRight;
 
         let imgAngle0 = new Image();
         let imgAngle90 = new Image();
@@ -41,11 +44,11 @@ LayupDrawer.prototype = {
         let positionTier5Y = layup.t1.thickness + layup.t2.thickness + layup.t3.thickness + layup.t4.thickness;
         imgAngle0.onload = function(){
             // DRAW TIER 1
-            ctx.drawImage(imgAngle0, 0, positionTier1Y, canvas.width, layer1Thickness);
+            ctx.drawImage(imgAngle0, paddingLeft, positionTier1Y, canvasWidth, layer1Thickness);
             // DRAW TIER 3
-            ctx.drawImage(imgAngle0, 0, positionTier3Y, canvas.width, layer3Thickness);
+            ctx.drawImage(imgAngle0, paddingLeft, positionTier3Y, canvasWidth, layer3Thickness);
             // DRAW TIER 5
-            ctx.drawImage(imgAngle0, 0, positionTier5Y, canvas.width, layer5Thickness);
+            ctx.drawImage(imgAngle0, paddingLeft, positionTier5Y, canvasWidth, layer5Thickness);
         };
 
         // TIER 2
@@ -58,48 +61,139 @@ LayupDrawer.prototype = {
             let widthHalfImage = imgAngle90.width / 2;
             let heightImage = imgAngle90.height;
             // DRAW IMAGE TIER 2
-            ctx.drawImage(imgAngle90, widthHalfImage, 0, widthHalfImage, heightImage, 0, positionTier2Y, canvas.width/8, layer2Thickness);
-            ctx.drawImage(imgAngle90, canvas.width/8 - 0.1, positionTier2Y, canvas.width/4, layer2Thickness);
-            ctx.drawImage(imgAngle90, canvas.width/8 * 3 - 0.1, positionTier2Y, canvas.width/4, layer2Thickness);
-            ctx.drawImage(imgAngle90, canvas.width/8 * 5 - 0.1, positionTier2Y, canvas.width/4, layer2Thickness);
-            ctx.drawImage(imgAngle90, 0, 0, widthHalfImage, heightImage, canvas.width/8 * 7 - 0.1, positionTier2Y, canvas.width/8, layer2Thickness);
+            ctx.drawImage(imgAngle90, widthHalfImage, 0, widthHalfImage, heightImage, paddingLeft, positionTier2Y, canvasWidth/8, layer2Thickness);
+            ctx.drawImage(imgAngle90, canvasWidth/8 - 0.1 + paddingLeft, positionTier2Y, canvasWidth/4, layer2Thickness);
+            ctx.drawImage(imgAngle90, canvasWidth/8 * 3 - 0.1 + paddingLeft, positionTier2Y, canvasWidth/4, layer2Thickness);
+            ctx.drawImage(imgAngle90, canvasWidth/8 * 5 - 0.1 + paddingLeft, positionTier2Y, canvasWidth/4, layer2Thickness);
+            ctx.drawImage(imgAngle90, 0, 0, widthHalfImage, heightImage, canvasWidth/8 * 7 - 0.1 + paddingLeft, positionTier2Y, canvasWidth/8, layer2Thickness);
             // DRAW TIER 4
-            ctx.drawImage(imgAngle90, widthHalfImage, 0, widthHalfImage, heightImage, 0, positionTier4Y, canvas.width/8, layer4Thickness);
-            ctx.drawImage(imgAngle90, canvas.width/8 - 0.1, positionTier4Y, canvas.width/4, layer4Thickness);
-            ctx.drawImage(imgAngle90, canvas.width/8 * 3 - 0.1, positionTier4Y, canvas.width/4, layer4Thickness);
-            ctx.drawImage(imgAngle90, canvas.width/8 * 5 - 0.1, positionTier4Y, canvas.width/4, layer4Thickness);
-            ctx.drawImage(imgAngle90, 0, 0, widthHalfImage, heightImage, canvas.width/8 * 7 - 0.1, positionTier4Y, canvas.width/8, layer4Thickness);
+            ctx.drawImage(imgAngle90, widthHalfImage, 0, widthHalfImage, heightImage, paddingLeft, positionTier4Y, canvasWidth/8, layer4Thickness);
+            ctx.drawImage(imgAngle90, canvasWidth/8 - 0.1 + paddingLeft, positionTier4Y, canvasWidth/4, layer4Thickness);
+            ctx.drawImage(imgAngle90, canvasWidth/8 * 3 - 0.1 + paddingLeft, positionTier4Y, canvasWidth/4, layer4Thickness);
+            ctx.drawImage(imgAngle90, canvasWidth/8 * 5 - 0.1 + paddingLeft, positionTier4Y, canvasWidth/4, layer4Thickness);
+            ctx.drawImage(imgAngle90, 0, 0, widthHalfImage, heightImage, canvasWidth/8 * 7 - 0.1 + paddingLeft, positionTier4Y, canvasWidth/8, layer4Thickness);
         };
-        
+        // Source Image 
         imgAngle0.src = 'images/paralel-grain-0.jpg';
         imgAngle90.src = 'images/perpendicular-grain-90.jpg';
+        
+        // Draw Line Bottom 
         let bottom = layup.t1.thickness + layup.t2.thickness + layup.t3.thickness + layup.t4.thickness + layup.t5.thickness;
-        this.drawLine(0, bottom, canvas.width, bottom, '#000', 1.5);
+        this.drawLine(0, bottom + 1, canvas.width, bottom + 1, '#000', 0.1);
+
+        this.drawRuler(0, 500, bottom, 30);
+
+        // Draw Ruler Bottom 
+        this.drawRulerBottom(bottom, canvasWidth);
+        // Draw Ruler Left
+        this.drawRulerLeft(bottom, canvasWidth);
     },
 
     /**
      * Add more functions as you need
      */
 
-    drawLine : function (x1, y1, x2,y2, stroke = 'black', width = 1) {
+    /**  @param     x1 = x Start 
+                    y1 = y start
+                    x2 = x finish
+                    y2 = y finish
+                    stroke = color line
+                    width = thickness of line 
+    */
+    drawLine : function (x1, y1, x2,y2, stroke = '#000', width = 1) {
         let canvas = this.canvas;
         let ctx = canvas.getContext('2d');
-        // start a new path
         ctx.beginPath();
-
-        // place the cursor from the point the line should be started 
         ctx.moveTo(x1, y1);
-
-        // draw a line from current cursor position to the provided x,y coordinate
         ctx.lineTo(x2, y2);
-
-        // set strokecolor
         ctx.strokeStyle = stroke;
-
-        // set lineWidht 
         ctx.lineWidth = width;
-
-        // add stroke to the line 
         ctx.stroke();
+    },
+    // @param      x1 = x start
+    //             x2 = x finish
+    //             space = interval 
+    //             stroke = warna 
+    //             lineWidth = width line
+    drawRuler : function (x1, x2, y, space, stroke='#000',lineWidth=0.1, startY = 700) {
+        let canvas = this.canvas;
+        let context = canvas.getContext('2d');
+        let spacing = 20;
+        context.lineWidth= lineWidth;
+        context.strokeStyle = stroke;
+        context.beginPath();
+        for(let interval = 40; interval < 450; interval+= 10)
+        {
+            context.moveTo(interval*spacing+0.5, y);
+            context.lineTo(interval*spacing+0.5, y+15);
+            context.stroke();
+        }
+    },
+    drawRulerBottom : function (totalThickness, canvasWidth) {
+        let canvas = this.canvas;
+        let context = canvas.getContext("2d");
+        
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        context.strokeStyle = "#000";
+        context.lineWidth = 0.5;
+
+        context.beginPath();
+        context.moveTo(30, totalThickness);
+        context.lineTo(canvas.width, totalThickness);
+        context.stroke();
+
+        let numberRuler = 0;
+        let posNumber = 0;
+        for (let i = 0; i <= 25; i++) {
+            let space = (canvasWidth/25);
+
+            context.beginPath();
+            context.moveTo((i*space) + 40, totalThickness);
+            context.lineTo((i*space) + 40, totalThickness + 10);
+            context.stroke();
+            
+            if (i % 5 == 0) {
+                if(numberRuler == 0){
+                    posNumber = ((i*space) + 38);
+                }else if(numberRuler < 99){
+                    posNumber = ((i*space) + 34);
+                }else{
+                    posNumber = ((i*space) + 30);
+                }
+                context.fillText(numberRuler.toString(), posNumber, totalThickness + 20);
+                numberRuler += 30;
+            }
+        }
+        // Primary Direction
+        context.fillText("Primary Direction", canvasWidth/2, totalThickness + 50);
+    },
+    drawRulerLeft: function (totalThickness, canvasWidth) {
+        let canvas = this.canvas;
+        let context = canvas.getContext("2d");
+        let height = totalThickness;
+        // Bersihkan canvas
+        context.clearRect(0, 0, canvas.width, height);
+
+        // Atur properti garis
+        context.strokeStyle = "#000";
+        context.lineWidth = 0.5;
+
+        // Gambar garis utama
+        context.beginPath();
+        context.moveTo(15, 0);
+        context.lineTo(15, height);
+        context.stroke();
+
+        // Gambar garis-garis kecil setiap 10 piksel
+        for (var i = 0; i <= height; i += 10) {
+            context.beginPath();
+            context.moveTo(10, i);
+            context.lineTo(20, i);
+            context.stroke();
+
+            // Tambahkan angka pada setiap 50 piksel
+            context.fillText(i.toString(), 5, i + 5);
+        }
     }
 };
